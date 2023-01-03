bbd50ad3e5b5f69149f44045c069748c4a08a93b
Eduard Camilo Ortega Sanchez <codecos.info@gmail.com>
2023-01-03 17:01:37 -0500

    react hook en boton

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(){
+    const [restaurantName, setRestaurantName] = useState("Lemon");
 
-export default App;
+    function updateRestaurantName(){
+        setRestaurantName("Little Lemon")
+    };
+
+    return (
+        <div>
+            <h1>{restaurantName}</h1>
+            <button onClick={updateRestaurantName}>
+                Update restaurant name
+            </button>
+        </div>
+    );
+};
